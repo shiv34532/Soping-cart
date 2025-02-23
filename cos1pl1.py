f2080de8b14493d9974e27224bbeb30730286b56
shiv34532 <gohelshivam983@gmail.com>
2025-02-23 11:04:27 +0530

    Python for  script to read your name, branch, email, and birth-date. print those details on the screen

---
+++ cos1pl1.py
@@ -0,0 +1,10 @@
+#Write a Python script to read your name, branch, email, and birth-date. print those details on the screen
+name=(input("enter your name:"))
+branch=(input("enter your branch:"))
+email=(input("enter your email:"))
+dob=(input("enter your dob:"))
+
+print("yourname is:",name)
+print("youer baranch is:",branch)
+print("enter your email:",email)
+print("enter your dob(date of birth:",dob)
