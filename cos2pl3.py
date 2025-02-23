e2218561c379d7e6ee0a81cf3160ef728459de34
shiv34532 <gohelshivam983@gmail.com>
2025-02-23 12:34:35 +0530

    Pyscript to  given integer number is odd or even
    Write a Python script to check whether given integer number is odd or even.
    even output :
    enter a number:4
    the  given number is even number: 4
    
    
    odd output is :
    enter a number:7
    the given number is odd number:7

---
+++ cos2pl3.py
@@ -0,0 +1,10 @@
+#Write a Python script to check whether given integer number is odd or even.
+NUMBER=int(input ("enter a number:"))
+if NUMBER%2==0:
+    print("the  given number is even number:",NUMBER)
+else:
+    print("the given number is odd number:",NUMBER)
+    
+    
+    
+    
