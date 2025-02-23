760e98dae043d1ae065ec3df2004df4ca8e3c091
shiv34532 <gohelshivam983@gmail.com>
2025-02-23 11:47:49 +0530

    slope ofline between two points x1, y1 and x2, y2
    enter x1 number3
    enter your oter second numbber:4
    enter iner fist number:5
    enter ineer second number:6
    the slope of line is: 1.0

---
+++ cos1pl4.py
@@ -0,0 +1,8 @@
+#Write a Python script to compute the slope of a line between two points (x1, y1) and (x2,
+
+x1=int(input("enter x1 number"))
+y1=int(input("enter your oter second numbber:"))
+x2=int(input("enter iner fist number:"))
+y2=int(input("enter ineer second number:"))
+slope=(y2-y1)/(x2-x1)
+print("the slope of line is:",slope)
