2b24b17cb97478e4908e49e8de033369417e10fe
shiv34532 <gohelshivam983@gmail.com>
2025-02-23 11:29:55 +0530

    Pyscript temperature from Celsius to Fahrenheit.
    Write a Python script to convert temperature from Celsius to Fahrenheit.
    Equation to convert Celsius to Fahrenheit: 𝐹 = (9/5) ∗ 𝐶 + 32.
    otput is:
    enter your what to you in yor city fehrenit:45
    The celsiusis : c 113.0

---
+++ cos1pl3.py
@@ -0,0 +1,5 @@
+#Write a Python script to convert temperature from Celsius to Fahrenheit.
+#Equation to convert Celsius to Fahrenheit: 𝐹 = (9/5) ∗ 𝐶 + 32.
+celsius=int(input("enter your what to you in yor city fehrenit:"))
+fehrenheit=(9/5)*celsius+32
+print("The celsiusis : c",fehrenheit)
